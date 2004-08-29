fd368c9e39088ee2e6c7447a3792d365c47abea8
William Pitcock <nenolod@dereferenced.org>
2004-08-29 06:51:07 +0000

    Change some formatting

--- src/callbacks.c
+++ src/callbacks.c
@@ -2,25 +2,27 @@
  * NetworkBOPM: The ShadowIRCd Anti-Proxy Solution.
  * callbacks.c: Callbacks from the OPM context.
  *
- * $Id: callbacks.c,v 1.5 2004/08/29 06:48:12 nenolod Exp $
+ * $Id: callbacks.c,v 1.6 2004/08/29 06:51:07 nenolod Exp $
  */
 
 #include "netbopm.h"
 
+extern int sockfd;
+
 void
-cb_open_proxy(char *ip)
+cb_open_proxy(char *ip, char *type)
 {
     sendto_server
-	(":%s KLINE * 86400 * %s :Open Proxy found on your host. Please visit www.blitzed.org/proxy?ip=%s for more information.",
-	 bopmuid, ip, ip);
-    snoop("[AUTOSCAN] Found open proxy for %s. Banning.", r->ip);
+	(sockfd, ":%s KLINE * 86400 * %s :Open %s Proxy found on your host.",
+	 bopmuid, ip, type);
+    snoop("[AUTOSCAN] Found open %s proxy on %s. Banning.", type, ip);
     is_complete = 1;
 }
 
 void
 cb_end(char *ip)
 {
-    snoop("[AUTOSCAN] Proxyscan complete for %s.", r->ip);
+    snoop("[AUTOSCAN] Proxyscan complete for %s.", ip);
     is_complete = 1;
 }
 
